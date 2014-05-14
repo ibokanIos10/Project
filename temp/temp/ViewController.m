a8e396ad7116641d110a496ae367e0836bbfc6f6
ibokan <ibokan@huangyujie.local>
2014-05-14 21:58:02 +0800

    temp

---
+++ temp/temp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  temp
+//
+//  Created by ibokan on 14-5-14.
+//  Copyright (c) 2014年 yujie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
